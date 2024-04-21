1d65f93c95461442c0bbdbb824b40ceccf747551
RaduRaresGeorge <rradu@myuax.com>
2024-04-21 16:12:23 +0200

    Command

---
+++ Command/src/main/java/commands/Command.java
@@ -0,0 +1,22 @@
+package src.main.java.commands;
+
+import src.main.java.editor.Editor;
+
+public abstract class Command {
+    public Editor editor;
+    private String backup;
+
+    Command(Editor editor) {
+        this.editor = editor;
+    }
+
+    void backup() {
+        backup = editor.textField.getText();
+    }
+
+    public void undo() {
+        editor.textField.setText(backup);
+    }
+
+    public abstract boolean execute();
+}
